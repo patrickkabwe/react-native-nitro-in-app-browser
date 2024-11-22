571c50708f6771547f8435d5ffbf5e3d84c399d1
Patrick Kabwe <patrickckabwe@gmail.com>
2024-11-22 13:25:26 +0200

    chore: clean up

--- package/src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './specs/NitroInAppBrowser.nitro';
-import { NitroModules } from 'react-native-nitro-modules';
-export const NitroInAppBrowser = NitroModules.createHybridObject('NitroInAppBrowser');
--- package/src/specs/NitroInAppBrowser.nitro.js
+++
@@ -1,6 +0,0 @@
-import {} from 'react-native-nitro-modules';
-export var NitroInAppBrowserPresentationStyle;
-(function (NitroInAppBrowserPresentationStyle) {
-    NitroInAppBrowserPresentationStyle["PageSheet"] = "pageSheet";
-    NitroInAppBrowserPresentationStyle["FormSheet"] = "formSheet";
-})(NitroInAppBrowserPresentationStyle || (NitroInAppBrowserPresentationStyle = {}));
